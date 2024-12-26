28eb8ea5d5358a8d67efb407a6d73ca0d7ad2f6b
LuthfilHD <akaishuichi141516@gmail.com>
2024-12-26 21:54:05 +0700

    import dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'], // Menambahkan font Roboto ke dalam tema
+      },
+    },
   },
   plugins: [],
 };
